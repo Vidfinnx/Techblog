959bc45c5fd4484f5ab5447aab3eb87f9df1467f
Veikko Aho <vidfinnx@yahoo.com>
2021-07-07 17:16:38 -0700

    clean up and formatting

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,12 +1,11 @@
-// this function corresponds to the add-post form
 async function newFormHandler(event) {
     event.preventDefault();
   
-    // capture the values of a form considered to be "add-post"
+
     const title = document.querySelector('input[name="post-title"]').value;
     const content = document.querySelector('textarea[name="content"]').value;
   
-    // post stringified data to the post-routes folder of our routes
+ 
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
@@ -18,7 +17,7 @@ async function newFormHandler(event) {
       }
     });
   
-    // once our route is received properly, re-direct user to dashboard
+
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
